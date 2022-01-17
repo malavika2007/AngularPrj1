cf7bd72023c19a14b4ca64a113329ea84515d41b
malavika2007 <lajivv@gmail.com>
2022-01-17 13:11:13 +0530

    herok sever deploying code added

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StudentComponent } from './student/student.component';
+import { TeacherComponent } from './teacher/teacher.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StudentComponent,
+    TeacherComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [TeacherComponent]
+})
+export class AppModule { }
