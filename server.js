2bdcbdcd11236013bc9c96a8a78d0a26fc0cf7ea
malavika2007 <lajivv@gmail.com>
2022-01-17 14:36:50 +0530

    new

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-//      
+app.use(express.static(__dirname + '/dist/erp-sw'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/ERP-sw/index.html'));
+res.sendFile(path.join(__dirname+'/dist/erp-sw/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
